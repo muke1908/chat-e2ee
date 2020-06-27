6ccf9be2153c97cdfcde8ee57d4703ac7f51a2de
mueksh <mukeshbsws@gmail.com>
2020-06-27 18:07:51 +0300

    feat: captcha validator added additional feat: husky commit linter prettier added

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const verifyCaptcha = require('../external/recaptcha');
+
 const router = express.Router({ mergeParams: true });
 
-router.get('/', async(req, res) => {
-    res.send({ message: 'working!' })
-})
+router.get('/', async (req, res) => {
+  res.send({ message: '/api -- working!' });
+});
+
+router.post('/getLink', async (req, res) => {
+  const { token } = req.body;
+  const captcha = await verifyCaptcha(token);
+
+  if (!captcha.success) {
+    return res.sendStatus(400);
+  }
+
+  // implement link generation
+  return res.send({ status: 'OK' });
+});
 
 module.exports = router;
