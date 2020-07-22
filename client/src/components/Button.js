d9f7eacc1d2c9877735dd11bd7453aa0562f0739
Gagan Aryan <62807661+gagan-aryan@users.noreply.github.com>
2020-07-22 18:58:28 +0530

    improvement: included the reusable button #24 (#25)
    * improvement: included the reusable button #24

---
+++ client/src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Button = (props) => {
+  const { label, type = 'primary', disabled = false, onClick, width } = props;
+
+  const styles = {
+    background: type === 'secondary' ? '#4caf50' : '#3944bc',
+    color: '#fff',
+    padding: '10px',
+    width: width || 'auto',
+    cursor: disabled === true ? '' : 'pointer',
+    diplay: 'inline-block',
+    borderRadius: '2px',
+    fontSize: '16px',
+    opacity: disabled === true ? '0.5' : '1.0',
+    fontFamily: 'inherit'
+  };
+
+  return (
+    <button style={styles} type={type} disabled={disabled} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
