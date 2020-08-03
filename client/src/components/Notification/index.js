3158d1833dbfd7432ebbb7f666ec807d08d88190
Radha Jayaraman <radhajayaraman11@gmail.com>
2020-08-03 22:47:47 +0530

    feat: audio component update

--- client/src/components/Notification/index.js
+++ client/src/components/Notification/index.js
@@ -8,10 +8,10 @@ const Notification = (props) => {
   const [play, setPlay] = React.useState(props.play);
 
   if (play) {
-    setPlay(false);
+    setPlay((play) => !play);
     playAudio();
     if (!play) {
-      setPlay(true);
+      setPlay((play) => !play);
       playAudio();
     }
   }
