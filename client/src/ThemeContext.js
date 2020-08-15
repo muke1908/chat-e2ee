1e76555639666622ad49ec5073767091d1f95337
Gagan Aryan <62807661+gagan-aryan@users.noreply.github.com>
2020-08-15 20:33:10 +0530

    Theme save (#78)
    * fix: theme save issue #65
    
    * chore: code smells
    
    * fix: useEffect used
    
    * fix: useEffect
    
    * refactor: minor changes
    
    * chore: code smell

--- client/src/ThemeContext.js
+++ client/src/ThemeContext.js
@@ -1,9 +1,12 @@
 import React, { useState, createContext } from 'react';
+import storage from './utils/storage.js';
 
 export const ThemeContext = createContext();
 
+const theme = storage.get('theme');
+
 export const ThemeProvider = (props) => {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(theme);
   return (
     <ThemeContext.Provider value={[darkMode, setDarkMode]}>{props.children}</ThemeContext.Provider>
   );
