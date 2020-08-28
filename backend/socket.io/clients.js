f6e566bc39be6293a94f84b1777e8c6d006b9740
mueksh <mukeshbsws@gmail.com>
2020-08-28 15:33:13 +0300

    feat: replaced pubnub, used websocket internally

---
+++ backend/socket.io/clients.js
@@ -0,0 +1,45 @@
+const clientRecord = {
+  /*
+    channelID: {
+        userID1: {
+            sid: <sid>
+        },
+        userID2: {
+            sid: <sid>
+        }
+    }
+    */
+};
+
+const clients = {
+  getClients: () => clientRecord,
+  getClientsByChannel: (channelID) => {
+    if (!channelID) {
+      throw new Error('channelID - required param');
+    }
+    return clientRecord[channelID];
+  },
+  getReceiverByChannel: (channelID, userID) => {
+    if (!(channelID && userID)) {
+      throw new Error('channelID, userID - required param');
+    }
+    const users = Object.keys(clientRecord[channelID]);
+
+    const receiver = users.find((u) => u !== userID);
+    return clientRecord[channelID][receiver];
+  },
+  setClientToChannel: (userID, channelID, sid) => {
+    if (clientRecord[channelID]) {
+      clientRecord[channelID][userID] = { sid };
+    } else {
+      clientRecord[channelID] = {
+        [userID]: { sid }
+      };
+    }
+  },
+  deleteClient: (userID, channelID) => {
+    delete clientRecord[channelID][userID];
+  }
+};
+
+module.exports = clients;
