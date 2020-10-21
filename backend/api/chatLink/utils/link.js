510460078248390b75b55c3487e0405585889e42
Parth <parth4321patel@gmail.com>
2020-10-21 19:29:27 +0530

    fix: add const for pinLength (#179)
    * fix: add const for pinLength
    
    * Update link.js
    
    Co-authored-by: Parth Patel <pptem@amazon.com>
    Co-authored-by: Mukesh <mukeshbsws@gmail.com>

--- backend/api/chatLink/utils/link.js
+++ backend/api/chatLink/utils/link.js
@@ -1,18 +1,20 @@
 const uuidv4 = require('uuid').v4;
-const { generatePIN } = require('./pin')
+const { generatePIN } = require('./pin');
 
-const chatLinkDomain = process.env.CHAT_LINK_DOMAIN;
+const CHAT_LINK_DOMAIN = process.env.CHAT_LINK_DOMAIN;
+const PIN_LENGTH = 4;
 
 const generateLink = () => {
   const hash = uuidv4();
+  
   return {
     hash,
     link: `/chat/${hash}`,
-    absoluteLink: `${chatLinkDomain}/chat/${hash}`,
+    absoluteLink: `${CHAT_LINK_DOMAIN}/chat/${hash}`,
     expired: false,
     deleted: false,
-    pin : generatePIN(hash, pinLength = 4),
-    pinCreatedAt : new Date().getTime()
+    pin: generatePIN(hash, PIN_LENGTH),
+    pinCreatedAt: new Date().getTime()
   };
 };
 
