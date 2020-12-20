626568b5028176e262cdc630c72e0867c355f466
GJ-16 <12jagadeesh4567@gmail.com>
2020-12-20 19:01:04 +0530

    imgur

--- backend/external/imagebb.js
+++ backend/external/imagebb.js
@@ -1,8 +1,22 @@
 const fetch = require('node-fetch');
 const FormData = require('form-data');
 
-const API_KEY = process.env.IMAGE_BB_API_KEY;
-const api = `https://api.imgbb.com/1/upload`;
+const IMGUR = 'imgur'
+const IMGBB = 'imgbb'
+
+var Hosting = IMGBB;
+
+
+var API_KEY = process.env.IMAGE_BB_API_KEY;
+var api = `https://api.imgbb.com/1/upload`;
+var headers = ''
+var url = `${api}?expiration=600&key=${API_KEY}`;
+
+if(Hosting === 'imgur'){
+  API_KEY = process.env.IMGUR_CLIENT_ID
+  url = `https://api.imgur.com/3/image`
+  headers = { "Authorization": `Client-ID ${API_KEY}` }
+}
 
 const uploadImage = async (base64) => {
   if (!API_KEY) {
@@ -16,18 +30,19 @@ const uploadImage = async (base64) => {
 
   const form = new FormData();
   form.append('image', imageData);
-  const url = `${api}?expiration=600&key=${API_KEY}`;
-
   const response = await fetch(url, {
     method: 'POST',
+    headers: headers,
     body: form
   });
   const imageResponse = await response.json();
-
   try {
-    return {
+    return Hosting == 'imgbb' ?  {
       imageurl: imageResponse.data.image.url
-    };
+    } 
+      :  {
+        imageurl: imageResponse.data.link
+      }
   } catch (err) {
     throw new Error('failed to send image');
   }
