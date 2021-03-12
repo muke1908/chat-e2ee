a166035074a36030adcce7cb0c064fe078d41d57
Prasoon Jain <54218922+prasoonjain4422@users.noreply.github.com>
2021-03-12 22:25:37 +0530

    Added zooming facility to the images. (#190)
    * Added zooming facility to the images.
    
    * Added zooming facility to the images - revised

--- client/src/components/Image/index.js
+++ client/src/components/Image/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 const Image = ({ src, maxHeight, maxWidth }) => {
+  const [modalState, setModalState] = useState('none');
+
   const style = {
     objectFit: 'contain',
     maxHeight,
     maxWidth
   };
 
-  return <img src={src} style={style} alt="" />;
+  return (
+    <div>
+      <img
+        src={src}
+        style={style}
+        alt=""
+        onClick={() => {
+          setModalState('block');
+        }}
+      />
+      <Modal style={{ height: '100%', width: '100%', display: modalState }} centered>
+        <img
+          src={src}
+          style={style}
+          alt=""
+          onClick={() => {
+            setModalState('none');
+          }}
+        />
+      </Modal>
+    </div>
+  );
 };
 
 export default Image;
