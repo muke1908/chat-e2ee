6151a8693679be090e5c823b32a68157308c3b53
Mukesh <mukeshbsws@gmail.com>
2021-09-26 18:54:59 +0200

    Update client/src/pages/messaging/helpers/index.js

--- client/src/pages/messaging/helpers/index.js
+++ client/src/pages/messaging/helpers/index.js
@@ -12,7 +12,8 @@ export {
 
 export const getUserSessionID = (channelID) => {
   const userID_storage = storage.get('session-user-uuid') || {};
-  return userID_storage.channelID === channelID ? userID_storage.userId : null;
+const userId = userID_storage.channelID === channelID ? userID_storage.userId : null;
+return userId;
 };
 
 export const createUserSessionID = (channelID) => `${channelID}-${new Date().getTime()}`;
