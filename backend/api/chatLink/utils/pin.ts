26c9bbc8f9db0142fb833658cb3dea3e8dde6621
Alireza <ali.r.riahi@gmail.com>
2023-01-02 21:41:11 +0330

    fix sonarcloud random warning

--- backend/api/chatLink/utils/pin.ts
+++ backend/api/chatLink/utils/pin.ts
@@ -1,37 +1,30 @@
-import { createHash } from 'crypto';
+import { createHash, randomInt } from "crypto";
 
-
-const base36map = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const base36map = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 export const generatePIN = (uuid, pinLength = 4) => {
-
-    /*
+  /*
         This function generates a unique PIN given the UUID. The parameters are:
         uuid => A string which can be uuid (the chat-hash in this case)
         pinLength => length of the unique PIN to generate, default is 4
     */
-    
-    //generate MD5 hash in hex representation
-    const md5HashInt = parseInt(
-        createHash('md5').update(uuid).digest('hex'), 16
-    );
 
-    //get mod 36 values
-    const rems = [];
-    let n = md5HashInt;
-    while( n > 0 ) {
-        rems.push(n % 36);
-        n = Math.floor( n / 36 );
-    } 
+  //generate MD5 hash in hex representation
+  const md5HashInt = parseInt(createHash("md5").update(uuid).digest("hex"), 16);
 
-    //randomly map K indexes to characters in base36map 
-    const randomChars = [];
-    for(var i = 0; i < pinLength; i++) {
-        randomChars.push(
-            base36map[ rems[ (Math.random() * rems.length) | 0] ]
-        );
-    }
+  //get mod 36 values
+  const rems = [];
+  let n = md5HashInt;
+  while (n > 0) {
+    rems.push(n % 36);
+    n = Math.floor(n / 36);
+  }
 
-    return randomChars.join("");
-}
+  //randomly map K indexes to characters in base36map
+  const randomChars = [];
+  for (let i = 0; i < pinLength; i++) {
+    randomChars.push(base36map[rems[(randomInt(0, 10000) * rems.length) | 0]]);
+  }
 
+  return randomChars.join("");
+};
