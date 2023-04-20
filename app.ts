65a9f222901bb92a79147dbff9c2acdf27a524df
Mukesh <mukeshbiswas@microsoft.com>
2023-04-20 13:09:41 +0200

    fix: path in app.ts

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ app.use("/api", apiController);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 } else {
   app.get("/*", (req, res) => {
