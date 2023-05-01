7de348b74d12a60b0204a0e74e17a7b7c6e35779
Estefania Sanchez <estefaniasanchez@Estefanias-Work-MacBook-Air.local>
2023-05-01 17:21:13 -0400

    finished  tests

--- backend/api/chatLink/utils/validateChannel.test.ts
+++ backend/api/chatLink/utils/validateChannel.test.ts
@@ -1,24 +1,63 @@
-// test if channel is provided otherwise throw new Error("channel - required param");
-// test that a valid channel returns valid: true
-// test that an invalid channel returns valid: false
-// test that a non-existent channel returns valid: false, state: CHANNEL_STATE.NOT_FOUND
-// Test that the function handles database errors gracefully
-// Test that the function calls the expected database function with the correct parameters
-
-// For const { expired, deleted } = ifExists;
-// Test cases that cover the following scenarios:
-// expired is true and deleted is false
-// expired is false and deleted is true
-// Both expired and deleted are true
-// Neither expired nor deleted are true
-// For each scenario, check that the expected state and valid properties are returned by the function.
-  
-// Test the return value, example:
-// test('channelValid returns expected object', async () => {
-//     const ifExists = { expired: true, deleted: false };
-//     const result = await channelValid('my_channel_id', ifExists);
-//     expect(result.valid).toBe(false);
-//     expect(result.state).toBe(CHANNEL_STATE.EXPIRED);
-// });
-  
 import channelValid, { CHANNEL_STATE } from './validateChannel';
+import db from "../../../db";
+
+jest.mock('../../../db');
+
+describe('channelValid', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    // test if channel is provided otherwise throw new Error("channel - required param");
+    it('should throw an error is the channel is not provided', async () => {
+        await expect(channelValid('')).rejects.toThrow("channel - required param");
+    });
+
+    it('should return NOT_FOUND if the channel does not exist in the db', async () => {
+        const findOneFromDBMock = db.findOneFromDB as jest.MockedFunction<typeof db.findOneFromDB>;
+        findOneFromDBMock.mockResolvedValueOnce(null);
+        // WHere is the above being used?
+        const result = await channelValid('nonexistent_channel');
+        expect(result.valid).toBe(false);
+        expect(result.state).toBe(CHANNEL_STATE.NOT_FOUND);
+    });
+
+    it('should return ACTIVE if the channel exists in the database and it is not expired or deleted', async () => {
+        const channel = {
+            hash: 'active_channel',
+            expired: false,
+            deleted: false
+        }
+        const findOneFromDBMock = db.findOneFromDB as jest.MockedFunction<typeof db.findOneFromDB>;
+        findOneFromDBMock.mockResolvedValueOnce(channel);
+        const result = await channelValid(channel.hash);
+        expect(result.valid).toBe(true);
+        expect(result.state).toBe(CHANNEL_STATE.ACTIVE);
+    });
+
+    it('should return deleted if the channel exists in the database and it is deleted', async () => {
+        const channel = {
+            hash: 'deleted_channel',
+            expired: false,
+            deleted: true
+        }
+        const findOneFromDbMock = db.findOneFromDB as jest.MockedFunction<typeof db.findOneFromDB>;
+        findOneFromDbMock.mockResolvedValueOnce(channel);
+        const result = await channelValid(channel.hash);
+        expect(result.valid).toBe(false);
+        expect(result.state).toBe(CHANNEL_STATE.DELETED);
+    });
+
+    it('should return expired if the channel exists in the database and it is expired', async () => {
+        const channel = {
+            hash: 'expired_channel',
+            expired: true,
+            deleted: false
+        }
+        const findOneFromDbMock = db.findOneFromDB as jest.MockedFunction<typeof db.findOneFromDB>;
+        findOneFromDbMock.mockResolvedValueOnce(channel);
+        const result = await channelValid(channel.hash);
+        expect(result.valid).toBe(false);
+        expect(result.state).toBe(CHANNEL_STATE.EXPIRED);
+    });
+});
