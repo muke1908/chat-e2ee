1613b9087f70f64e6c20faa65848e571dcfa8188
Mukesh <mukeshbiswas@microsoft.com>
2023-05-09 18:40:40 +0200

    fix: type declaration

--- service/src/public/types.ts
+++ service/src/public/types.ts
@@ -30,6 +30,6 @@ export interface ICryptoUtils {
 
 export declare const createChatInstance: () => IChatE2EE;
 export declare const generateUUID: () => string;
-export declare const cryptoUtils: () => IChatE2EE;
+export declare const cryptoUtils: () => ICryptoUtils;
 
 
