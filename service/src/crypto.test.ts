c5b272d30160f4d3e05e0ef538af5f43c840ca93
Estefania SanchezCabrejos <esanchezcabrejos@ncf.edu>
2023-06-30 15:05:55 -0400

    Tests finished

--- service/src/crypto.test.ts
+++ service/src/crypto.test.ts
@@ -2,38 +2,34 @@ import e from "cors";
 import { cryptoUtils } from "./crypto";
 
 describe('cryptoUtils', () => {
-  // Browser dependencies mock
-  const mockEncryptionResult = new ArrayBuffer(8);
-  const mock64BaseString = 'ZW5jcnlwdGVkLXRleHQ=';
+  const mockBase64String = 'ZW5jcnlwdGVkLXRleHQ='; // decoded = encrypted-text
   const subtle = {
+    // Generates a new key (for symmetric algorithms) or key pair (for public-key algorithms).
+    // Returns a promise that fulfills with a CryptoKey (for symmetric algorithms) or a CryptoKeyPair (for public-key algorithms).
     generateKey: jest.fn().mockResolvedValue({
       publicKey: 'public-key',
       privateKey: 'private-key',
     }),
-    // exportKey: jest.fn().mockImplementation((type, str) => str),
-    exportKey: jest.fn().mockImplementation(function (type, str) {
-      console.log('exportKey: ', str);
-      return str;
-    }),
-    // importKey: jest.fn().mockImplementation((type, str) => str),
-    importKey: jest.fn().mockImplementation(function (type, str) {
-      console.log('importKey: ', str);
-      return str;
-    }),
-    encrypt: jest.fn().mockImplementation(function (algorithm, publicKey, encoded) { 
-      console.log('algo: ', algorithm);
-      console.log('publicKey: ', publicKey);
-      console.log('encoded: ', encoded);
-      return mockEncryptionResult;
-    })
+    // Takes as input a CryptoKey object and gives you the key in an external, portable format.
+    // Returns a promise - If format was jwk, then the promise fulfills with a JSON object containing the key.
+    exportKey: jest.fn().mockImplementation((type, str) => str),
+    // Takes as input a key in an external, portable format and gives you a CryptoKey object that you can use in the Web Crypto API.
+    // Returns a promise that fulfills with the imported key as a CryptoKey object.
+    importKey: jest.fn().mockImplementation((type, str) => str),
+    // It takes as its arguments a key to encrypt with, some algorithm-specific parameters, and the data to encrypt (also known as "plaintext").
+    // Returns a promise that fulfills with an ArrayBuffer containing the "ciphertext".
+    encrypt: jest.fn().mockImplementation((algorithm, publicKey, encoded) => encoded),
+    decrypt: jest.fn().mockImplementation((algorithm, privateKey, ciphertext) => ciphertext)
   };
 
+  // Receives a Uint8Array to encode to base-64 
+  // Returns ASCII string containing the Base64 representation of stringToEncode.
   const btoa = jest.fn().mockImplementation(function (str) {
-    console.log('str received in btoa: ', str);
-    const base64string = Buffer.from(str).toString('base64');
-    console.log('base64string created with Buffer: ', base64string);
-    return mock64BaseString;
-    // (str) => 'ZW5jcnlwdGVkLXRleHQ=');
+    return mockBase64String;
+  });
+  // Receives a base-64 encoded string to decode to Uint8Array
+  const atob = jest.fn().mockImplementation(function (str) {
+    return "This is another message";
   });
 
   let window = {} as any;
@@ -42,6 +38,7 @@ describe('cryptoUtils', () => {
   } as any;
 
   window.btoa = btoa;
+  window.atob = atob;
 
   beforeEach(() => {
     global.window = window;
@@ -62,44 +59,24 @@ describe('cryptoUtils', () => {
     it('should encrypt plaintext using a public key and return a base64-encoded string', async () => {
       const plaintext = 'This is a message';
       const { publicKey } = await cryptoUtils.generateKeypairs();
-      console.log('From encryptMessage test. This is the publicKey generated: ', publicKey);
       const ciphertext = await cryptoUtils.encryptMessage(plaintext, publicKey);
-      console.log('From encryptMessage test. This is the ciphertext generated: ', ciphertext);
+
       expect(window.crypto.subtle.importKey).toHaveBeenCalledWith('jwk', expect.any(String), { name: 'RSA-OAEP', hash: 'SHA-256' }, true, ['encrypt']);
-      
+      expect(window.btoa).toHaveBeenCalledWith(expect.any(String));
       expect(ciphertext).toBe('ZW5jcnlwdGVkLXRleHQ=');
-      // expect(ciphertext).toMatch(/^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/);
-
-      // expect(ciphertext).toBeTruthy;
-
-      
-      // expect(ciphertext).not.toBeNull;
-
-      
-      // expect(ciphertext).toBeNull;
     });
-
-    // it('should produce different ciphertexts for different plaintexts', async () => {
-    //   const plaintext1 = 'First message';
-    //   const plaintext2 = 'Second message';
-
-    //   const { publicKey } = await cryptoUtils.generateKeypairs();
-    //   const ciphertext1 = await cryptoUtils.encryptMessage(plaintext1, publicKey);
-    //   const ciphertext2 = await cryptoUtils.encryptMessage(plaintext2, publicKey);
-
-    //   expect(ciphertext1).not.toBe(ciphertext2);
-    // });
   });
 
-  // describe('decryptMessage', () => {
-  //   it('should decrypt a ciphertext using a private key', async () => {
-  //     const plaintext = 'This is another message';
+  describe('decryptMessage', () => {
+    it('should decrypt a ciphertext using a private key', async () => {
+      const plaintext = 'This is another message';
+      const keyPair = await cryptoUtils.generateKeypairs();
+      const ciphertext = await cryptoUtils.encryptMessage(plaintext, keyPair.publicKey);
+      const decryptedText = await cryptoUtils.decryptMessage(ciphertext, keyPair.privateKey);
 
-  //     const keyPair = await cryptoUtils.generateKeypairs();
-  //     const ciphertext = await cryptoUtils.encryptMessage(plaintext, keyPair.publicKey);
-  //     const decryptedText = await cryptoUtils.decryptMessage(ciphertext, keyPair.privateKey);
-
-  //     expect(decryptedText).toBe(plaintext);
-  //   });
-  // });
+      expect(window.crypto.subtle.importKey).toHaveBeenCalledWith('jwk', expect.any(String), { name: 'RSA-OAEP', hash: 'SHA-256' }, true, ['encrypt']);
+      expect(window.atob).toHaveBeenCalledWith(expect.any(String));
+      expect(decryptedText).toBe("This is another message");
+    });
+  });
 });
