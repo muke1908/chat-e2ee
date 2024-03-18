0e7ae7931639f041920b45f8512926cc1b409308
Emilio Bravo <emilioberumen910@gmail.com>
2024-03-18 18:36:20 -0400

    Creation of the minimalistic UI on client. The code provided is a modification of the front end apparence of the LinkDisplay and ChatLink Components. Before Continuing with the text messaging section would like to have this part approved first.

--- client/src/components/LinkDisplay/index.tsx
+++ client/src/components/LinkDisplay/index.tsx
@@ -25,39 +25,32 @@ const LinkDisplay: React.FC<{ content: LinkObjType }> = ({ content }) => {
   };
 
   return (
-    <div className={styles.fullWidth}>
-      <div className={styles.divider} />
-      <span className={styles.pinDisplayMsg}>Anyone with the Link can join your chat</span>
-      <div
-        className={`${styles.copyToClipboardContainer}
-        ${!darkMode && styles.lightModeContainer}`}
-      >
-        <span className={styles.labelLinkTextArea}>Share chat link: </span>
-        <FiLink className={styles.linkIcon} />
-        <div className={styles.textAreaContainer}>
-          <input
-            ref={textAreaRef}
-            value={chatLink}
-            onClick={selectText}
-            className={`${styles.linkTextArea}
-            ${!darkMode && styles.lightTextArea}`}
-            readOnly
-          />
-        </div>
-        <div>
-          <button
-            className={`${styles.copyButton}
-            ${!darkMode && styles.lightModeButton}`}
-            onClick={copyCodeToClipboard}
-          >
-            <FiCopy className={styles.copyIcon} /> {buttonText}
-          </button>
-          <button
-            type="button"
-            className={`${styles.qrButton} ${!darkMode && styles.lightModeButton}`}
-            onClick={() => setShowQR(!showQR)}
-          >
-            {showQR ? (
+    <>
+    <div className={styles.card}>
+    <h1 className={styles.title}>Anyone with the Link can join your chat</h1>
+    <p className={styles.subtitle}>Share chat link:</p>
+    <div  className={styles.input} >
+    <input 
+      ref={textAreaRef}
+      value={chatLink}
+      onClick={selectText}
+      className={`${styles.linkTextArea} ${!darkMode && styles.lightTextArea}`}
+      readOnly
+     />
+    </div>
+    <div className={styles.flexRow}>
+      
+      <button 
+        className={`${styles.button} ${styles.copyButton}`}
+        onClick={copyCodeToClipboard}>
+        <FiCopy className={styles.copyIcon} /> 
+        {buttonText}
+      </button>
+      
+      <button 
+        className={`${styles.button} ${styles.qrButton}`}
+        onClick={() => setShowQR(!showQR)}>
+           {showQR ? (
               <div className={styles.QrCodeContent}>
                 QR Code <FiArrowUp className={styles.qrIcon} />
               </div>
@@ -66,25 +59,30 @@ const LinkDisplay: React.FC<{ content: LinkObjType }> = ({ content }) => {
                 QR Code <FiArrowDown className={styles.qrIcon} />
               </div>
             )}
-          </button>
-        </div>
-      </div>
-      <div className={styles.divider} />
-      <div
-        className={`${styles.openLink}
-      ${darkMode ? styles.darkOpenLink : styles.lightOpenLink}`}
-      >
-        <a href={`${chatLink}`} target="_blank" rel="noopener noreferrer">
-          Open chat <FiExternalLink />
-        </a>
-      </div>
-      {showQR && (
-        <div className={styles.qrCodeContainer}>
-          <QRCode value={chatLink} size={128} />
-        </div>
-      )}
+        </button>
     </div>
-  );
+
+  </div>
+  
+  <div className={styles.divider} />
+    <div
+      className={`${styles.openLink}
+    ${darkMode ? styles.darkOpenLink : styles.lightOpenLink}`}
+    >
+      <a href={`${chatLink}`} target="_blank" rel="noopener noreferrer">
+        Open chat <FiExternalLink />
+      </a>
+    </div>
+    {showQR && (
+      <div className={styles.qrCodeContainer}>
+        <QRCode value={chatLink} size={128} />
+      </div>
+    )}
+    
+  </>
+);
+
+
 };
 
 export default LinkDisplay;
--- client/src/pages/chatlink/index.tsx
+++ client/src/pages/chatlink/index.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import LinkDisplay from "../../components/LinkDisplay/index";
-import { ThemeContext } from "../../ThemeContext";
-import styles from "./Style.module.css";
 import ThemeToggle from "../../components/ThemeToggle/index";
 
 import { createChatInstance, LinkObjType } from "@chat-e2ee/service";
 
+import styles from "./Style.module.css";
+
+
 const App = () => {
   const [chatLink, setChatLink] = useState<LinkObjType>(null);
   const [loading, setLoading] = useState(false);
-  const [darkMode] = useContext(ThemeContext);
 
   const generateLink = async () => {
     if (loading) {
@@ -30,68 +30,56 @@ const App = () => {
       setLoading(false);
     }
   };
-
   return (
     <>
-      <div className={styles.linkGenerationPage}>
-        <div
-          className={`${styles.header}
-          ${darkMode === true ? styles.darkModeHeader : styles.lightModeHeader}`}
-        >
-          🔐 Disposable e2ee-chat
-          <ThemeToggle />
-        </div>
-        <div className={`${styles.sectionDefault} ${!darkMode && styles.sectionDefaultLight}`}>
-          <div className={styles.title}>
-            Secure, end-to-end encrypted environment for exchanging sensitive information with peer.
-          </div>
-          <div className={styles.description}>
-            <ul>
-              <li>No login/ signup required.</li>
+      <div className={styles.flexContainer}>
+        <div />
+        <main className={styles.mainContent}>
+          <div className={styles.subContent}>
+            <h1 className={styles.title}>🔐 Disposable e2ee-chat 🔐</h1>
+            <ThemeToggle />
+            <p className={styles.paragraph}>
+              Secure, end-to-end encrypted environment for exchanging sensitive information with peer.
+            </p>
+            <ul className={styles.list}>
+              <li>No login/ signup required</li>
               <li>No tracker</li>
               <li>
-                Your messages are <b>end-to-end</b> encrypted - technically impossible to read your
-                messages by someone else.
+                Your messages are end-to-end encrypted - technically impossible to read your messages by someone else.
               </li>
             </ul>
           </div>
           {!chatLink && (
-            <div className={styles.linkGenerationBtnContainer}>
-              <br />
-              <Button
-                label={loading?"Creating...":"Create chat link"}
-                type="primary"
-                onClick={generateLink}
-                disabled={loading}
-              />
-            </div>
+             <div className={styles.linkGenerationBtnContainer}>
+             <br />
+             <Button
+               label={loading?"Creating...":"Create chat link"}
+               type="primary"
+               onClick={generateLink}
+               disabled={loading}
+             />
+           </div>
           )}
-          {chatLink && (
+           {chatLink && (
             <div className={styles.captchaHeightSetter}>
               <LinkDisplay content={chatLink} />
             </div>
           )}
-        </div>
-        <div
-          className={`${styles.sectionContribute} ${
-            darkMode === true ? styles.sectionDefault : styles.sectionDefaultLight
-          }`}
-        >
-          <div className={styles.title}>
-            ❤️ The source-code is public on&nbsp;
-            <a
-              href="https://github.com/muke1908/chat-e2ee"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-            , feel free to contribute!
+
+          <div className="flex justify-center items-center w-full">
+            <div className={styles.infoBox}>
+              <p className={styles.paragraph}>
+                ❤️ The source-code is public on Github, feel free to contribute!
+              </p>
+            </div>
           </div>
-        </div>
+        </main>
+        <div />
       </div>
     </>
   );
-};
+}
+
+
 
 export default App;
