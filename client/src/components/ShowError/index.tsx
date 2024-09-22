8c2a38d9716935443fc0e8814ed90dfa48523e6e
Amey Sanyal <amzicom123@gmail.com>
2024-09-22 13:13:57 +0530

    fix: added keyboard events

--- client/src/components/ShowError/index.tsx
+++ client/src/components/ShowError/index.tsx
@@ -4,21 +4,38 @@ import { MdClose } from "react-icons/md";
 
 type ShowErrorProps = {
   errorMessage: string;
-  onClose: (event: React.MouseEvent<HTMLDivElement>) => void;
+  onClose: (
+    event: React.MouseEvent<HTMLDivElement | HTMLSpanElement> | React.KeyboardEvent<HTMLDivElement>
+  ) => void;
 };
 
 const ShowError = (props: ShowErrorProps) => {
-  console.log(`in show error compo ${typeof props.errorMessage}`);
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter" || e.key === " ") {
+      props.onClose(e);
+      e.stopPropagation();
+    }
+  };
+
   return (
-    <div className={styles.bg} onClick={props.onClose}>
+    <div
+      className={styles.bg}
+      onClick={props.onClose}
+      tabIndex={-1}
+      onKeyDown={handleKeyDown}
+      role="button"
+    >
       <div
+        role="button"
+        tabIndex={0}
         className={styles.err}
         onClick={(e) => {
           e.stopPropagation();
         }}
+        onKeyDown={handleKeyDown}
       >
         <h3 className={styles.heading}>Error</h3>
-        <span onClick={props.onClose}>
+        <span onClick={props.onClose} role="button" tabIndex={0} onKeyDown={handleKeyDown}>
           <MdClose className={styles.closeIcon} title="close" size={25} />
         </span>
         <p>{props.errorMessage}</p>
