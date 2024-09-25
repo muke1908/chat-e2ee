04fa8c762a092b18377cb0c9dec3aa4a70531653
会飞的猫 <admin@hfdem.com>
2024-09-25 23:33:09 +0800

    style: fix Copied button misalignment on mobile browsers (#382)
    * style: fix Copied button misalignment on mobile browsers
    
    * perf: enhance QR code clarity using QRCodeSVG (#381)
    
    * build: update dependencies (#383)

--- client/src/components/LinkDisplay/index.tsx
+++ client/src/components/LinkDisplay/index.tsx
@@ -1,9 +1,11 @@
-import React, { useRef, useState, useContext } from "react";
-import { FiLink, FiCopy, FiExternalLink, FiArrowDown, FiArrowUp } from "react-icons/fi";
+import React, { useRef, useState, useContext, useEffect } from "react";
+import { FiLink, FiCopy, FiExternalLink } from "react-icons/fi";
+import { MdOutlineQrCode } from "react-icons/md";
 import styles from "./Style.module.css";
 import { ThemeContext } from "../../ThemeContext";
 import { LinkObjType } from "@chat-e2ee/service";
 import { QRCodeSVG } from "qrcode.react";
+import detectMobile from "../../utils/detectMobile";
 
 const LinkDisplay: React.FC<{ content: LinkObjType }> = ({ content }) => {
   const chatLink =
@@ -13,6 +15,11 @@ const LinkDisplay: React.FC<{ content: LinkObjType }> = ({ content }) => {
   const [buttonText, setButtonText] = useState("Copy");
   const [showQR, setShowQR] = useState(false);
   const [darkMode] = useContext(ThemeContext);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(detectMobile());
+  }, []);
 
   const copyCodeToClipboard = () => {
     if (textAreaRef.current !== null) textAreaRef.current.select();
@@ -50,22 +57,16 @@ const LinkDisplay: React.FC<{ content: LinkObjType }> = ({ content }) => {
             ${!darkMode && styles.lightModeButton}`}
             onClick={copyCodeToClipboard}
           >
-            <FiCopy className={styles.copyIcon} /> {buttonText}
+            <FiCopy className={styles.copyIcon} /> {!isMobile && buttonText}
           </button>
           <button
             type="button"
             className={`${styles.qrButton} ${!darkMode && styles.lightModeButton}`}
             onClick={() => setShowQR(!showQR)}
           >
-            {showQR ? (
-              <div className={styles.QrCodeContent}>
-                QR Code <FiArrowUp className={styles.qrIcon} />
-              </div>
-            ) : (
-              <div className={styles.QrCodeContent}>
-                QR Code <FiArrowDown className={styles.qrIcon} />
-              </div>
-            )}
+            <div className={styles.QrCodeContent}>
+              <MdOutlineQrCode className={styles.qrIcon} /> {!isMobile && "QR Code"}
+            </div>
           </button>
         </div>
       </div>
